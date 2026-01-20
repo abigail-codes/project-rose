ed652c3adb0c1abaaf46ece63651ee36308cf15c
Abigail <abigailtan32406@gmail.com>
2026-01-20 17:56:13 +0800

    Add files via upload

---
+++ js/story.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const scrollUpBtn = document.getElementById('scroll-up');
+
+    function scrollUp() {
+       
+        if (window.scrollY >= 300) {
+            scrollUpBtn.classList.add('show-scroll');
+        } else {
+            scrollUpBtn.classList.remove('show-scroll');
+        }
+    }
+
+    window.addEventListener('scroll', scrollUp);
+
+    const navToggle = document.getElementById('burger');
+    const navMenu = document.getElementById('nav-menu');
+
+    if (navToggle && navMenu) {
+        navToggle.addEventListener('click', () => {
+            navMenu.classList.toggle('show-menu');
+        });
+    }
+
+    const navLinks = document.querySelectorAll('li a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            navMenu.classList.remove('show-menu');
+        });
+    });
+});
+
